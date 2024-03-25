a50f3180976248bd9ed17aa9e1c8e6b374e7d344
JubiJung <jubi1838@gmail.com>
2024-03-25 20:18:07 +0900

    today commit

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import NextAuth from "next-auth/next";
 import NaverProvider from "next-auth/providers/naver";
 import KakaoProvider from "next-auth/providers/kakao";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 dotenv.config();
 
@@ -18,10 +17,5 @@ export default NextAuth({
       clientSecret: process.env.KAKAO_SECRET!,
     }),
   ],
-  // callbacks: {
-  //   async session({ session, token, user }) {
-  //     session.user!.name = token.sub as string;
-  //     return session;
-  //   },
-  // },
+  secret: process.env.NEXTAUTH_SECRET,
 });
