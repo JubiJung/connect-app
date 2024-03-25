592dccebe68feb4c7996aa4bb574665c566b3ec9
JubiJung <jubi1838@gmail.com>
2024-03-25 19:45:42 +0900

    today commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   important: true,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/meetup/[meetupId]/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     container: {
